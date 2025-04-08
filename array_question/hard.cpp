5c73b3d555969db7afee41a60df098f6f29030b5
Tejas1234-biradar <biradartejas842@gmail.com>
2025-04-08 22:07:47 +0530

    hard questions begin

---
+++ array_question/hard.cpp
@@ -0,0 +1,85 @@
+#include<iostream>:
+#include<set>
+#include<unordered_map>
+#include<vector>
+using namespace std;
+vector<vector<int>> threeSum(vector<int> a){
+    vector<vector<int>> ans;
+    int n=a.size();
+    sort(a.begin(),a.end());
+    for(int i=0;i<n;i++){
+        if(i>0&&a[i]==a[i-1]) continue;
+        int j=i+1;
+        int k=n-1;
+        while(j<k){
+            int sum=a[i]+a[j]+a[k];
+            if(sum<0){
+                j++;
+            }
+            else if(sum==0){
+            vector<int> temp={a[i],[j],a[k]};
+            ans.push_back(temp);
+                j++;
+                k--;
+            }
+            else 
+                k--;
+        }
+    }
+    return ans;
+}
+vector<vector int>> fourSum(vector<int> a){
+    set<vector<int>> st;
+    int n= a.size();
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            for(int k=j+1;k<n;k++){
+                for(int l=k+1;l<n;l++){
+                long long sum=a[i]+a[j]
+                        sum+=a[k];
+                        sum+=a[l];
+                    if(sum==0){
+                        vector<int> temp={a[i],a[k],a[l],a[j]};
+                        sort(temp.begin(),temp.end());
+                        st.insert(temp);
+
+                    }
+                }
+            }
+        }
+    }
+    vector<vector<int>> ans(st.begin,st,end);
+    return ans;
+}
+/* breute force complexity of O(n*n*n)
+int longestSubarrayK(vector<int> a){
+    int n=a.size();
+    int res=0;
+    int cnt=0;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+        for(int k=i;k<j;k++){
+                res=0;
+                res=res^a[k];
+                if(res==k) cnt++;
+            }    
+        }
+        return cnt;
+    }
+}*/ 
+int maximumSubarrayXor(vector<int> a,int k){
+    int n=a.size();
+    int res=0;
+    int maxLen=0;
+    unordered_map<int, int> prefixXor;
+    for(int i=0;i<n;i++){
+    res^=a[i];
+    if(res==k) maxLen=i+1;
+    if(prefixXor.find(res^k)!=prefixXor.end()){
+            maxLen=max(maxLen,i-prefixXor[res^k]);
+        }
+ if(prefixXor.find(res^k)==prefixXor.end())   prefixXor[res]=i;
+    }
+return maxLen;
+}
+
